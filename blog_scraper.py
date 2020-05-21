fd3eb6d6494bce61b2077cff1922ae91b1dc4cf3
brian <brianbb174@protonmail.com>
2020-05-21 12:59:56 -0700

    add title scrape

---
+++ blog_scraper.py
@@ -0,0 +1,11 @@
+# https://www.rithmschool.com/blog
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+response = requests.get("https://www.rithmschool.com/blog")
+
+soup =BeautifulSoup(response.text, "html.parser")
+articles = soup.find_all("article")
+for article in articles:
+    title = print(article.find("a").get_text())
